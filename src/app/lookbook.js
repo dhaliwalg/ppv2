b3beb110fdda9d5aec88df83e7e7b3216b0da01a
Gurjit Dhaliwal <55417421+dhaliwalg@users.noreply.github.com>
2023-11-11 19:51:21 -0500

    more photos

--- src/app/lookbook.js
+++ src/app/lookbook.js
@@ -15,10 +15,16 @@ export default function Lookbook() {
                 <Box className="overflow-scroll mt-4 color-white scrollbar-hide">
                     <Stack ml='4' spacing={8} direction={['column', 'row']}>
                         <Image className="rounded-md" boxSize='205px' src='/img1.jpg' alt="sikh pagg" />
-                        <Image className="rounded-md" boxSize='205px' src='/img3.jpg' alt="sikh pagg" />
-                        <Image className="rounded-md" boxSize='205px' src='/img5.jpg' alt="bhangra pagg" />
                         <Image className="rounded-md" boxSize='205px' src='/img2.jpg' alt="wedding saafa" />
-                        <Image className="rounded-md" boxSize='205px' src='/img4.jpg' alt="groom party" />
+                        <Image className="rounded-md" boxSize='205px' src='/img6.jpg' alt="tying" />
+                        <Image className="rounded-md" boxSize='205px' src='/img9.jpg' alt="jaggi" />
+                        <Image className="rounded-md" boxSize='205px' src='/img4.jpg' alt="sikh pagg" />
+                        <Image className="rounded-md" boxSize='205px' src='/img5.jpg' alt="bhangra pagg" />
+                        <Image className="rounded-md" boxSize='205px' src='/img10.jpg' alt="saafa2" />
+                        <Image className="rounded-md" boxSize='205px' src='/img8.jpg' alt="greensikh" />
+                        <Image className="rounded-md" boxSize='205px' src='/img3.jpg' alt="groom party" />
+                        <Image className="rounded-md" boxSize='205px' src='/img7.jpg' alt="tying2" />
+                        <Image className="rounded-md" boxSize='205px' src='/img11.jpg' alt="saafa" />
                     </Stack>
                 </Box>
             </SlideFade>
