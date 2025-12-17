1f39e66fdfb1e438c1928709aefad8cb1389473a
Prabnoor Singh <prabi52@umd.edu>
2025-12-17 00:47:25 -0500

    poo

--- src/app/lookbook.js
+++ src/app/lookbook.js
@@ -33,7 +33,7 @@ export default function Lookbook() {
           }}
         >
           <Stack ml="4" spacing={8} direction={["column", "row"]}>
-            <Image
+            {/* <Image
               className="rounded-md"
               boxSize="205px"
               src="/img1.jpg"
@@ -44,7 +44,7 @@ export default function Lookbook() {
               boxSize="205px"
               src="/img2.jpg"
               alt="wedding saafa"
-            />
+            /> */}
             <Image
               className="rounded-md"
               boxSize="205px"
@@ -69,18 +69,18 @@ export default function Lookbook() {
               src="/img5.jpg"
               alt="bhangra pagg"
             />
-            <Image
+            {/* <Image
               className="rounded-md"
               boxSize="205px"
               src="/img10.jpg"
               alt="saafa2"
-            />
-            <Image
+            /> */}
+            {/* <Image
               className="rounded-md"
               boxSize="205px"
               src="/img8.jpg"
               alt="greensikh"
-            />
+            /> */}
             <Image
               className="rounded-md"
               boxSize="205px"
@@ -93,12 +93,12 @@ export default function Lookbook() {
               src="/img7.jpg"
               alt="tying2"
             />
-            <Image
+            {/* <Image
               className="rounded-md"
               boxSize="205px"
               src="/img11.jpg"
               alt="saafa"
-            />
+            /> */}
           </Stack>
         </Box>
       </SlideFade>
